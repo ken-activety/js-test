b9d8bb863fb763e9d979b500ca7c5f9191dbb69e
ken-activety <kennerous4@gmail.com>
2025-03-05 14:54:15 +0100

    finish

--- ken.js
+++ ken.js
@@ -1,47 +1,46 @@
-// var name;
-// var age;
+ var name;
+ var age;
 
-// name = prompt('Enter whit your name');
-// age = prompt('enter your age');
-// console.log(' welcome ' + name + ' How old are you ' + age + ' ans');
-
-
-// function TotalCost(costPerPerson, numberOfPeople) {
-//   return costPerPerson - numberOfPeople;
-// }
-// console.log(TotalCost(100, 55));
+ name = prompt('Enter whit your name');
+ age = prompt('enter your age');
+ console.log(' welcome ' + name + ' How old are you ' + age + ' ans');
 
 
+ function TotalCost(costPerPerson, numberOfPeople) {
+   return costPerPerson - numberOfPeople;
+ }
+ console.log(TotalCost(100, 55));
 
 
-// function checkDiscount(age) {
-//   if (age > 18) {
-//     console.log("You are eligible for a discount!");
-//   } else {
-//     console.log("Sorry, no discount for you.");
-//   }
-// }
-// checkDiscount(16);
+
+ function checkDiscount(age) {
+   if (age > 18) {
+     console.log("You are eligible for a discount!");
+   } else {
+     console.log("Sorry, no discount for you.");
+   }
+ }
+ checkDiscount(16);
 
 
-// let figuer = 99.99;
-// let roundedfiguer = Math.round(figuer);
-// console.log("Rounded figuer: " + roundedfiguer);
+ let figuer = 99.99;
+ let roundedfiguer = Math.round(figuer);
+ console.log("Rounded figuer: " + roundedfiguer);
 
 
-// let name = 'meveil';
-// let age = 42;
-// let paidregistration = true;
+ let name = 'meveil';
+ let age = 42;
+ let paidregistration = true;
 
-// console.log(typeof name);
-// console.log(typeof age);
-// console.log(typeof paidregistration);
+ console.log(typeof name);
+ console.log(typeof age);
+ console.log(typeof paidregistration);
 
 
-// let participants = ["Meveil", "steve", "nehemie"];
-// participants.push("ken");
+let participants = ["Meveil", "steve", "nehemie"];
+ participants.push("ken");
 
-// console.log(participants);
+ console.log(participants);
 
 
 
@@ -51,38 +50,7 @@ alert("Welcome, " + name + "!");
 
 
 
-// // node one
-// let email;
-// let age;
 
-// email = prompt('Enter your email address');
-// age = prompt('enter your age');
-// console.log(' welcome ' + email + ' your age ' + age + ' ans');
-
-
-// var home,cyti;
-// var age;
-
-// home,cyti = prompt('Enter whit your name');
-// age = prompt('enter your homecyti');
-// console.log(' welcome ' + home,cyti + ' name of your home cyti ' + ' your age ' + age + ' ans');
-
-
-// var name;
-// var age;
-
-// name = prompt('Enter whit your name');
-// age = prompt('enter your age');
-// console.log(' welcome ' + name + ' your age ' + age + ' ans');
-
-// // Step 1: Make a variable for the name
-// let name = prompt("What is your name?")
-
-// // Step 1: Make a variable for the name
-// let name = prompt("What is your name?")
-
-// // Step 3: Show a welcome message
-// console.log("Welcome, " + name + " You're  saking age " + age + " years old.");
 
 
 
